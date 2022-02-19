f6934deadd58bd9e38e7cbd17abe603b64fbb238
tarik-abraham <tarik.abraham97@gmail.com>
2022-02-19 10:46:11 -0500

    New update after review

--- src/codingTasks/CodingTask1.java
+++ src/codingTasks/CodingTask1.java
@@ -8,16 +8,24 @@ public class CodingTask1 {
 	 * Create an array on int values using a scanner 
 	 * and calculate the sum of all stored elements in that array.
 	 */
+	/*	1)Take the size of the Array using scanner class from the user
+	 *  2)Create an int array of the above size
+	 *  3)Fill the arrray with elements from the user using scanner class
+	 *  4)Sum all the elements of the array
+	 *  
+	 */
 	
 	
 	public static void main(String[] args) {
 		
 		Scanner input=new Scanner(System.in);
-		
+		// prompt the user for the size of Array
 		System.out.println("Please enter the length of your array");
-		
+		// take the size from the user and store it in the size variable
 		int size=input.nextInt();
+		//create an int array of above size
 		int [] nums=new int[size];
+		// fill the array with elements from the user using scanner class
 		int sum = 0;
 		
 		
@@ -28,7 +36,7 @@ public class CodingTask1 {
 		}System.out.println("The sum of all of your stored elements in this array is " +sum);
 		
 		
-		
+		//System.out.printn(Arrays.toString(array));// prints out the elements of an array with out loops
 		
 		
 		
